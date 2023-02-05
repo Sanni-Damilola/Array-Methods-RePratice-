0f58b0acf3525b487a2e140b6a1c7e269e1cfaac
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-05 09:22:27 +0100

    pratice

---
+++ index.ts
@@ -0,0 +1,32 @@
+const companies = [
+  {
+    name: "EDHF",
+    category: "Charity",
+    start: 2020,
+    end: "internity",
+  },
+  {
+    name: "codeLab",
+    category: "Technolofy",
+    start: 2020,
+    end: "internity",
+  },
+  {
+    name: "Company 3",
+    category: "Retail",
+    start: 2020,
+    end: 2040,
+  },
+  {
+    name: "Company 4",
+    category: "Auto",
+    start: 2020,
+    end: 2010,
+  },
+  {
+    name: "Company 5",
+    category: "Finace",
+    start: 2020,
+    end: 2090,
+  },
+];
